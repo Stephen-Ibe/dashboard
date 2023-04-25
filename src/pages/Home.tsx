bc729b7b68602172e54bb6c2074f04ae144e230c
Stephen-Ibe <saintkalson@gmail.com>
2023-04-25 17:49:13 +0100

    sidebar menu adn layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import {
+  MdAlarm,
+  MdFilePresent,
+  MdGroup,
+  MdOutlineDashboard,
+  MdOutlineEdit,
+  MdOutlineHourglassEmpty,
+  MdSubscriptions,
+} from "react-icons/md";
+import { RiDeleteBin7Line } from "react-icons/ri";
+import { TbCameraPlus } from "react-icons/tb";
 import { Link } from "react-router-dom";
-import { MdOutlineDashboard } from "react-icons/md";
 import Logo from "../assets/images/mainstack-logo.png";
 
 const dateRanges = [
@@ -11,64 +21,79 @@ const dateRanges = [
   { id: 5, title: "Custom Date" },
 ];
 
+const SideMenuItems = [
+  { id: 0, title: "Dashboard", icon: <MdOutlineDashboard />, isActive: true },
+  { id: 1, title: "Item 1", icon: <MdOutlineEdit />, isActive: false },
+  { id: 2, title: "Item 2", icon: <MdGroup />, isActive: false },
+  {
+    id: 3,
+    title: "Item 3",
+    icon: <MdOutlineHourglassEmpty />,
+    isActive: false,
+  },
+  {
+    id: 4,
+    heading: "Others 1",
+    subItems: [
+      { id: 0, title: "Item 4", icon: <TbCameraPlus /> },
+      { id: 1, title: "Item 5", icon: <RiDeleteBin7Line /> },
+    ],
+  },
+  {
+    id: 5,
+    heading: "Others 2",
+    subItems: [
+      { id: 0, title: "Item 6", icon: <MdSubscriptions /> },
+      { id: 1, title: "Item 7", icon: <MdFilePresent /> },
+      { id: 1, title: "Item 8", icon: <MdAlarm /> },
+    ],
+  },
+];
+
 const Home = () => {
   return (
-    // <div className="h-screen flex">
-    //   <div className="border w-[20%] p-8 fixed top-0 left-0 h-screen">
-    //     <div className="logo">
-    //       <img src={Logo} alt="mainstack_logo" />
-    //     </div>
-    //     SM
-    //   </div>
-    //   <div className="w-full lg:w-[80%] p-4">
-    //     <div className="container mx-auto py-4 lg:px-10">
-    //       <div className="header">
-    //         <h1 className="font-bold text-xl">Dashboard</h1>
-    //       </div>
-    //       <main>
-    //         <div className="db--heading my-8 flex items-center justify-between">
-    //           <div className="space-y-1">
-    //             <h2 className="text-xl font-semibold">
-    //               Good Morning, Blessing ⛅️
-    //             </h2>
-    //             <p className="text-xs">Check out your dashboard summary.</p>
-    //           </div>
-    //           <Link to="/" className="text-secondary-orange text-sm">
-    //             View Analytics
-    //           </Link>
-    //         </div>
-    //         <div className="date--range">
-    //           {dateRanges.map(({ id, title, isActive }) => (
-    //             <div
-    //               className={`${isActive && "isActive"} date--range-pill`}
-    //               key={id}
-    //             >
-    //               {title}
-    //             </div>
-    //           ))}
-    //         </div>
-    //         <div className="border rounded-xl h-[500px] my-10">H</div>
-    //         <div className="border rounded-xl h-[500px] my-10">H</div>
-    //       </main>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="grid min-h-screen grid-cols-[auto_1fr] justify-center overflow-hidden">
-      <aside className="md:flex h-[calc(100vh_-_.0rem)] sm:w-28 lg:w-72 py-6 flex-col border-r hidden">
-        <div className="logo px-6">
-          <img src={Logo} alt="mainstack_logo" />
+      <aside className="md:flex h-[calc(100vh_-_.0rem)] sm:w-28 lg:w-72 py-6 flex-col justify-between border-r hidden">
+        <div>
+          <div className="logo px-6">
+            <img src={Logo} alt="mainstack_logo" />
+          </div>
+          <div className="my-10 border">
+            <ul className="space-y-5">
+              {SideMenuItems.map(
+                ({ id, title, icon, isActive, heading, subItems }) =>
+                  heading ? (
+                    <div className="px-6 pt-4">
+                      <h4 className="uppercase">{heading}</h4>
+                      <ul className="space-y-5 mt-4">
+                        {subItems.map(({ id, title, icon }) => (
+                          <li
+                            key={id}
+                            className="flex items-center gap-x-2 font-semibold"
+                          >
+                            {icon}
+                            {title}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ) : (
+                    <li
+                      key={id}
+                      className={`${
+                        isActive &&
+                        "border-secondary-orange text-secondary-orange border-l-4"
+                      } px-6 flex items-center gap-x-2 font-semibold`}
+                    >
+                      {icon}
+                      {title}
+                    </li>
+                  )
+              )}
+            </ul>
+          </div>
         </div>
-        <div className="my-10">
-          <ul className="">
-            <li className="px-6 border-l-4 border-secondary-orange font-semibold text-secondary-orange flex items-center gap-x-2">
-              <MdOutlineDashboard />
-              Dashboard
-            </li>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
-        </div>
+        <div>H</div>
       </aside>
       <div className="h-[calc(100vh_-_0rem)] w-full overflow-y-scroll">
         <main className="min-h-screen w-full py-6 px-10">
